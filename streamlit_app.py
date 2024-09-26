dce0bd218062d0abf6a062387519b41ca40e7e4f
LdyGray <159605933+LdyGray@users.noreply.github.com>
2024-09-25 21:18:50 -0400

    Added simple event from Google example
    https://google-calendar-simple-api.readthedocs.io/en/latest/getting_started.html

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,3 +56,30 @@ else:
         with st.chat_message("assistant"):
             response = st.write_stream(stream)
         st.session_state.messages.append({"role": "assistant", "content": response})
+
+from gcsa.event import Event
+from gcsa.google_calendar import GoogleCalendar
+from gcsa.recurrence import Recurrence, DAILY, SU, SA
+
+from beautiful_date import Jan, Apr
+
+
+calendar = GoogleCalendar('your_email@gmail.com')
+event = Event(
+    'Breakfast',
+    start=(1 / Jan / 2019)[9:00],
+    recurrence=[
+        Recurrence.rule(freq=DAILY),
+        Recurrence.exclude_rule(by_week_day=[SU, SA]),
+        Recurrence.exclude_times([
+            (19 / Apr / 2019)[9:00],
+            (22 / Apr / 2019)[9:00]
+        ])
+    ],
+    minutes_before_email_reminder=50
+)
+
+calendar.add_event(event)
+
+for event in calendar:
+    print(event)
